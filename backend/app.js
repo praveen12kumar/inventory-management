8c2b6e2947c189cb06a9065b7717ef7bf9c3a343
praveen kumar <praveenshakya1@gmail.com>
2024-03-05 23:44:50 +0530

    setup to deploy in on render

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,10 @@ import cors from "cors";
 import helmet from 'helmet';
 import itemRouter from './routes/itemRoutes.js';
 import saleRouter from './routes/saleRoute.js';
+import path from "path";
+config({path:"backend/config/config.env"});
 
-config({path:"backend/config/config.env"})
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -19,6 +21,12 @@ app.use(express.urlencoded({ extended:true }));
 // Routes
 app.use("/api/v1", itemRouter);
 app.use("/api/v1/sale", saleRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 
 export default app;
